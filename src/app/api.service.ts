a9e987864c93f379a944306b55140c13e773fa3d
kheanglov <lovsokheang@gmail.com>
2022-11-03 23:13:46 +0700

    update

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,13 @@ import { map } from 'rxjs/operators';
 import { environment } from "src/environments/environment";
 import { FILE_TYPE, getFormattedView, IVideo } from "./helper";
 
-const httpHeaders = new HttpHeaders({ 'Accept': '*/*' });
+const httpHeaders = new HttpHeaders({
+  'Accept': '*/*',
+  'Access-Control-Allow-Credentials': 'true',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+  'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+});
 
 @Injectable()
 export class ApiService {
